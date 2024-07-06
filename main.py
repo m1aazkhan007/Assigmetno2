1b5c0c589825181fa5a5c9796bf827dc498797e9
m1aazkhan007 <maazkhan8511@gmail.com>
2024-07-06 14:51:39 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# 1 Calculate the area of a triangle using the formula (0.5 ​× base x height)
+print("Find the area of a Triangle")
+base =int(input("Enter Triangle base "));
+height=int(input("Enter triangle height  "))
+areaOfTringle=(0.5*base*height)
+print("The Area of triangle is: ",areaOfTringle)
+                #   //
+                #   //
+                #   //fd
+                #  //
+                #  //
+
+# 2 Compute the area of a square using the formula (side x side).
+side=int(input("Enter side length of the square"))
+areaOfSquar=side**2 
+print("area of a square is:",areaOfSquar)
+                #   ///
+                #   ///
+                #   ///
+                #   ///
+                #   ///
+#  3 Find the area of a circle with a radius of 7 units. formula (π×radius*radius)
+pai=3.142;
+radius=7;
+areaOfCircle=(pai*radius*radius)
+print(areaOfCircle)
